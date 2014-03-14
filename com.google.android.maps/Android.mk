be476dc4567a095efb34595a52575a22191a91e4
Rene Treffer <treffer@measite.de>
2014-03-14 22:55:47 +0100

    Remove useless setting LOCAL_RESOURCE_DIR

--- com.google.android.maps/Android.mk
+++ com.google.android.maps/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := com.google.android.maps
 LOCAL_CERTIFICATE := platform
 LOCAL_SDK_VERSION := current
