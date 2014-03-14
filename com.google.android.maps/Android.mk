6e29153fb1eb5cf7ee8cdcdc25cb85858b12c861
Rene Treffer <treffer@measite.de>
2014-03-14 22:58:29 +0100

    Add missing LOCAL_MODULE for maps api

--- com.google.android.maps/Android.mk
+++ com.google.android.maps/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.android.maps
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := com.google.android.maps
@@ -14,6 +15,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := in/$(LOCAL_MODULE)
-
 include $(BUILD_PREBUILT)
 
